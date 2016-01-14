2a7a5d2bd93e29343bdb57cf9cccda5f9bbe0b1a
Robert Hajek <hajek.rob@gmail.com>
2016-01-14 09:14:35 -0700

    finished exercise

---
+++ prime-factors.js
@@ -0,0 +1,15 @@
+var findFactor = function(num) {
+  if (num <= 1) {return [];}
+  var i = 2;
+  while (num % i) {i++;}
+  var factors = [];
+  factors.push(i);
+  var addtl = findFactor(num / i);
+  if (addtl.length) {factors = factors.concat(addtl);}
+
+  return factors;
+};
+
+module.exports = primeFactors = {
+  for: findFactor
+};
